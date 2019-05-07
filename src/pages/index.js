ebb69c9666e18c0fb0d8a7a7a63b726a02c1517a
Lina <linachan.nz@gmail.com>
2019-05-07 22:11:34 +1000

    Added placeholders for header and cover

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,21 @@
 import React from "react"
 import { Link } from "gatsby"
+import styled from "styled-components"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
+const Section = styled.section`
+  height: 400px;
+  border: solid 1px blue;
+`
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+    <Section />
+
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
