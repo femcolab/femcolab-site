d21e5244c40ba0245ae7448e46678c6ae144b8ab
Duyen Ho <duyenho@gmail.com>
2019-05-07 21:37:30 +1000

    Update metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Female Coders Lab`,
+    description: `Monthly labs for women learning web development`,
+    author: `@femcolab`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
