369840c42fe5d94cf0994b9674be32eab2d4a1aa
Duyen Ho <duyenho@gmail.com>
2020-02-22 18:42:20 +1100

    Add menu and update layout

--- src/components/layout/View.tsx
+++ src/components/layout/View.tsx
@@ -1,10 +1,3 @@
-/**
- * Layout component that queries for data
- * with Gatsby's StaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/static-query/
- */
-
 import React, { ReactNode } from 'react'
 import { StaticQuery, graphql } from 'gatsby'
 import styled, { ThemeProvider } from 'styled-components'
@@ -12,38 +5,19 @@ import { GlobalStyles } from './styles/global'
 import { theme } from './styles/theme'
 
 import Burger from '../burger'
+import Menu from '../menu'
 // import Header from "../header"
 
 const Container = styled.div`
   margin: 0 auto;
   max-width: 1500px;
-  padding: 0px 1.0875rem 1.45rem;
-  padding-top: 0;
+  padding: 2rem;
   border: solid 1px red;
 `
 
 const Footer = styled.footer`
   height: 50px;
   background-color: pink;
-`
-const Menu = styled.nav`
-  height: 100vh;
-  width: 100vw;
-  background-color: tomato;
-  z-index: 20;
-`
-
-const List = styled.ul`
-  list-style: none;
-  text-align: center;
-  margin: 0;
-  padding: 100px;
-`
-
-const ListItem = styled.li`
-  a {
-    text-decoration: none;
-  }
 `
 
 type Props = {
@@ -65,26 +39,8 @@ const View = ({ children }: Props) => (
       <ThemeProvider theme={theme}>
         <GlobalStyles />
         <Burger />
+        <Menu />
         {/* <Header siteTitle={data.site.siteMetadata.title} /> */}
-        <Menu>
-          <List>
-            <ListItem>
-              <a href="#community">Community</a>
-            </ListItem>
-            <ListItem>
-              <a href="#members">Members</a>
-            </ListItem>
-            <ListItem>
-              <a href="/">Join Us</a>
-            </ListItem>
-            <ListItem>
-              <a href="#wemeetfor">We Meet For</a>
-            </ListItem>
-            <ListItem>
-              <a href="#gallery">Gallery</a>
-            </ListItem>
-          </List>
-        </Menu>
         <Container>
           <main>{children}</main>
           <Footer>FemCoLab 2019</Footer>
---
+++ src/components/menu/View.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { StyledMenu } from './styles'
+
+const View = () => {
+  return (
+    <StyledMenu>
+      <a href="#community">Community</a>
+      <a href="#members">Members</a>
+      <a href="/">Join us</a>
+      <a href="#wemeetfor">We Meet For</a>
+      <a href="#gallery">Gallery</a>
+    </StyledMenu>
+  )
+}
+export default View
