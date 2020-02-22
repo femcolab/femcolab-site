4d248802e048a00a87635aeb3d5dd83d33a70cb6
Duyen Ho <duyenho@gmail.com>
2020-02-22 17:37:37 +1100

    Add burger with new component file pattern
    Ref: https://bit.ly/2TiAS9B

---
+++ src/components/burger/View.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+import { StyledBurger } from "./styles"
+
+const View = () => {
+  return (
+    <StyledBurger>
+      <div />
+      <div />
+      <div />
+    </StyledBurger>
+  )
+}
+
+export default View
