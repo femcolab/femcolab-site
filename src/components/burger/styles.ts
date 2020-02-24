2c7ef5e7d172265d0170a678cb1e3bfcac5ea029
Duyen Ho <duyenho@gmail.com>
2020-02-24 22:40:13 +1100

    Change burger colour to white
    Todo: change colour conditionally on scroll

--- src/components/burger/styles.ts
+++ src/components/burger/styles.ts
@@ -22,9 +22,9 @@ export const StyledBurger = styled.button<any>`
   div {
     width: 2rem;
     height: 0.25rem;
-    background: ${({ theme, open }) =>
-      open ? theme.primaryLight : theme.primaryDark};
-    /* background: ${({ theme }) => theme.primaryDark}; */
+    /* background: ${({ theme, open }) =>
+      open ? theme.primaryLight : theme.primaryDark}; */
+    background: ${({ theme }) => theme.primaryLight};
     border-radius: 10px;
     transition: all 0.3s linear;
     position: relative;
