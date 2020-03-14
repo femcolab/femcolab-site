a5eac483b235d218e530fb02fcd05d6a76013c9d
Duyen Ho <duyenho@gmail.com>
2020-03-14 22:44:57 +1100

    add intersection observer for community intersecting logo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 import Layout from '../components/layout/View'
 import SEO from '../components/seo'
@@ -7,14 +7,36 @@ import Community from '../components/community'
 import Members from '../components/members'
 import Gallery from '../components/gallery'
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <Intro />
-    <Community />
-    <Members />
-    <Gallery />
-  </Layout>
-)
+const IndexPage = () => {
+  const [isIntersecting, setIsIntersecting] = useState(false)
+
+  let target: HTMLElement | null
+  const handleIntersect = (entries: any) => {
+    entries.forEach((entry: any) => {
+      entry.isIntersecting ? setIsIntersecting(true) : setIsIntersecting(false)
+    })
+  }
+
+  useEffect(() => {
+    const options = {
+      rootMargin: '1000px 0px -85%',
+      threshold: 0,
+    }
+    const observer = new IntersectionObserver(handleIntersect, options)
+
+    target = document.getElementById('community')
+    target && observer.observe(target)
+  }, [])
+
+  return (
+    <Layout isIntersecting={isIntersecting}>
+      <SEO title="Home" />
+      <Intro />
+      <Community />
+      <Members />
+      <Gallery />
+    </Layout>
+  )
+}
 
 export default IndexPage
